7945467a86c8ba2f99b36e2bff11a0d4d0d20e47
PrashantBhanage <prashantbhanage717@gmail.com>
2025-12-29 15:59:21 +0530

    Update and rename InformationHandler.java to BlogsHandler.java

--- src/server/BlogsHandler.java
+++ src/server/BlogsHandler.java
@@ -4,14 +4,14 @@ import com.sun.net.httpserver.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-public class InformationHandler implements HttpHandler {
+public class BlogsHandler implements HttpHandler {
 
     private static final String WEB_ROOT = "web";
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
-        File file = new File(WEB_ROOT, "information.html");
+        File file = new File(WEB_ROOT, "blogs.html");
         if (!file.exists()) {
             exchange.sendResponseHeaders(404, -1);
             return;
@@ -25,24 +25,23 @@ public class InformationHandler implements HttpHandler {
         String userName = WeatherServer.getUserFromSession(exchange);
         boolean loggedIn = userName != null;
 
-        // Replace username
         html = html.replace("{{USERNAME}}", loggedIn ? userName : "Guest");
 
-        // Active nav
+        // active nav
         html = html.replace("{{ACTIVE_HOME}}", "");
-        html = html.replace("{{ACTIVE_PROFILE}}", "");
-        html = html.replace("{{ACTIVE_INFO}}", "nav-link-active");
+        html = html.replace("{{ACTIVE_BLOGS}}", "nav-link-active");
+        html = html.replace("{{ACTIVE_INFO}}", "");
+        html = html.replace("{{ACTIVE_RANKINGS}}", "");
 
-        // Profile link
         if (loggedIn) {
             html = html.replace("{{PROFILE_LINK}}",
-                "<a href='/profile' class='nav-link'>Profile</a>");
+                    "<a href='/profile' class='nav-link'>Profile</a>");
             html = html.replace("{{AUTH_LINK}}",
-                "<a href='/logout' class='nav-link nav-link-danger'>Logout</a>");
+                    "<a href='/logout' class='nav-link nav-link-danger'>Logout</a>");
         } else {
             html = html.replace("{{PROFILE_LINK}}", "");
             html = html.replace("{{AUTH_LINK}}",
-                "<a href='/login' class='nav-link'>Login</a>");
+                    "<a href='/login' class='nav-link'>Login</a>");
         }
 
         byte[] bytes = html.getBytes(StandardCharsets.UTF_8);
@@ -52,4 +51,3 @@ public class InformationHandler implements HttpHandler {
         exchange.close();
     }
 }
-a
